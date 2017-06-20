e7cd35f69b0ccbc3232be303b5b7c013ec11f17a
ravi0402 <ravi.sharnagat@gmail.com>
2017-06-20 12:49:28 +0530

    2nd

--- server.js
+++ server.js
@@ -24,16 +24,21 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const hostname = 'localhost';
-const port = 3000;
 
-const server = app.listen(port, hostname, function() {
-  console.log("Server running at http://" + hostname +":" + port);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+app.use(function(err, req, res, next) {
 
-//app.use(express.static(__dirname + '/public'));
-
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
+
 
