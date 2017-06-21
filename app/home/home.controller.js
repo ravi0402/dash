bfbbb6988c4a449448e0ade87877c6f7f53abe09
ravi0402 <ravi.sharnagat@gmail.com>
2017-06-21 12:23:42 +0530

    3rd commit

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -18,51 +18,82 @@
     $scope.i=0;
     $scope.user_stats =function()
   {
+
+    var unix_seconds_tod = ((new Date()).getTime());
+    var diff = ((new Date()).getTime()) % 100000;
+    unix_seconds_tod = unix_seconds_tod - diff ;
+    var dateTod = { date : unix_seconds_tod };
+
+    var yesterday = new Date(new Date().setDate(new Date().getDate()-1));
+    var unix_seconds_yest = yesterday.getTime();
+    var diff = (yesterday.getTime()) % 100000;
+    unix_seconds_yest = unix_seconds_yest - diff ;
+    var dateYest = { date : unix_seconds_yest };
+
+    $http.post('http://production.dailyninja.in:8080/analytics',dateTod)
+        .success(function (data,status) {
+          $rootScope.totalAmountAnalyticsTod = data['totalAmountAnalytics'];
+          $rootScope.totalUserTod = data['totalUser'];
+          $rootScope.totalOrdersTod = data['totalOrders'];
+          $rootScope.milkRevenueTod = data['analyticsCategoryList'][0]['total'] + data['analyticsCategoryList'][4]['total'];
+          $rootScope.nonmilkRevenueTod = $rootScope.totalAmountAnalyticsTod - $rootScope.milkRevenueTod ;
+
+        });
+
+
+    $http.post('http://production.dailyninja.in:8080/analytics',dateYest)
+        .success(function (data,status) {
+          $rootScope.totalAmountAnalyticsYest = data['totalAmountAnalytics'];
+          $rootScope.totalUserYest = data['totalUser'];
+          $rootScope.totalOrdersYest = data['totalOrders'];
+          $rootScope.milkRevenueYest = data['analyticsCategoryList'][0]['total'] + data['analyticsCategoryList'][4]['total'];
+          $rootScope.nonmilkRevenueYest = $rootScope.totalAmountAnalyticsYest - $rootScope.milkRevenueYest ;
+          $scope.i=1;
+        });
+
     $http.get('/api/users1').success(function(data){
-      console.log(data);
         $rootScope.countUsers1 = data[0].ut;
         });
     $http.get('/api/users2').success(function(data){
         $rootScope.countUsers2 = data[0].uy;
         });
-    $http.get('/api/usersRechargedTod').success(function(data){
-        $rootScope.usersRechargedTod = data[0].a;
+    // $http.get('/api/usersRechargedTod').success(function(data){
+    //     $rootScope.usersRechargedTod = data[0].a;
+    //     });
+    // $http.get('/api/usersRechargedYest').success(function(data){
+    //     $rootScope.usersRechargedYest = data[0].a;
+
+    //     });
+     $http.get('/api/downloadsTod').success(function(data){
+        $rootScope.downloadsTod = data[0].downloadCount;
+
         });
-    $http.get('/api/usersRechargedYest').success(function(data){
-        $rootScope.usersRechargedYest = data[0].a;
+      $http.get('/api/downloadsYest').success(function(data){
+        $rootScope.downloadsYest = data[0].downloadCount;
+
+        });
+       $http.get('/api/subscriptionsTod').success(function(data){
+        $rootScope.subscriptionsTod = data[0].subscriptionCount;
+
+        });
+        $http.get('/api/subscriptionsYest').success(function(data){
+        $rootScope.subscriptionsYest = data[0].subscriptionCount;
+
         });
 
-    $http.get('/api/usersBilledTod').success(function(data){
-        $rootScope.usersBilledTodCount = data[0].billed_users;
-        $rootScope.usersBilledTodAmount = data[0].amount;
-        });
+    // $http.get('/api/usersBilledTod').success(function(data){
+    //     $rootScope.usersBilledTodCount = data[0].billed_users;
+    //     $rootScope.usersBilledTodAmount = data[0].amount;
+    //     });
 
-    $http.get('/api/usersBilledYest').success(function(data){
-        $rootScope.usersBilledYestCount = data[0].billed_users;
-        $rootScope.usersBilledYestAmount = data[0].amount;
-        $scope.i=1;
-        });
+    // $http.get('/api/usersBilledYest').success(function(data){
+    //     $rootScope.usersBilledYestCount = data[0].billed_users;
+    //     $rootScope.usersBilledYestAmount = data[0].amount;
+        
+    //     });
   }
   $scope.user_stats();
 
-    //$scope.imagePath = 'img/washedout.png';
-
-  // $scope.showAdvanced = function(ev) {
-
-  //   $mdDialog.show({
-  //     controller: DialogController,
-  //     templateUrl: 'pages/orders/orders.tmpl.html',
-  //     parent: angular.element(document.body),
-  //     targetEvent: ev,
-  //     clickOutsideToClose:true,
-
-  //   })
-  //   .then(function(answer) {
-  //     $scope.status = 'You said the information was "' + answer + '".';
-  //   }, function() {
-  //     $scope.status = 'You cancelled the dialog.';
-  //   });
-  // };
 
 $scope.showTabDialog = function(ev) {
     $mdDialog.show({
--- app_api/routes/api.js
+++ app_api/routes/api.js
@@ -52,7 +52,7 @@ router.get('/usersRechargedYest', function (req, res, next) {
 
 router.get('/usersBilledTod', function (req, res, next) {
 
-    client.query("SELECT COUNT(DISTINCT(user)) as billed_users, SUM(amount) as amount FROM daily_log WHERE created_at >= CURDATE() +'00:00:00';" , function(err, results) {
+    client.query("SELECT COUNT(DISTINCT(user)) as billed_users, SUM(amount) as amount FROM daily_log WHERE created_at >= CURDATE() +'00:00:00' AND daily_log_type='6ms8KZ3Kw5'" , function(err, results) {
         if (err)
         {
             throw err;
@@ -63,7 +63,7 @@ router.get('/usersBilledTod', function (req, res, next) {
 
 router.get('/usersBilledYest', function (req, res, next) {
 
-    client.query("SELECT COUNT(DISTINCT(user)) as billed_users, SUM(amount) as amount FROM daily_log WHERE created_at <= CURDATE() +'00:00:00' AND created_at >= CURDATE()-1 +'00:00:00';" , function(err, results) {
+    client.query("SELECT COUNT(DISTINCT(user)) as billed_users, SUM(amount) as amount FROM daily_log WHERE created_at <= CURDATE() +'00:00:00' AND created_at >= CURDATE()-1 +'00:00:00' AND daily_log_type='6ms8KZ3Kw5'" , function(err, results) {
         if (err)
         {
             throw err;
@@ -72,7 +72,49 @@ router.get('/usersBilledYest', function (req, res, next) {
     });
 });
 
+router.get('/downloadsTod', function (req, res, next) {
 
+    client.query("SELECT COUNT(*) as downloadCount FROM user u LEFT JOIN building b ON u.building = b.id WHERE u.created_at >= CURDATE() + '00:00:00';" , function(err, results) {
+        if (err)
+        {
+            throw err;
+        }
+        res.json(results);
+    });
+});
+
+router.get('/downloadsYest', function (req, res, next) {
+
+    client.query("SELECT COUNT(*) as downloadCount FROM user u LEFT JOIN building b ON u.building = b.id WHERE u.created_at <= CURDATE() +'00:00:00' AND u.created_at >= CURDATE()-1 +'00:00:00';" , function(err, results) {
+        if (err)
+        {
+            throw err;
+        }
+        res.json(results);
+    });
+});
+
+router.get('/subscriptionsTod', function (req, res, next) {
+
+    client.query("SELECT COUNT(*) as subscriptionCount FROM user u JOIN building b ON u.building = b.id JOIN billing_master bm ON u.billing_master_id  = bm.id WHERE u.created_at >=CURDATE() + '00:00:00' AND bm.amount < 0;" , function(err, results) {
+        if (err)
+        {
+            throw err;
+        }
+        res.json(results);
+    });
+});
+
+router.get('/subscriptionsYest', function (req, res, next) {
+
+    client.query("SELECT COUNT(*) as subscriptionCount FROM user u JOIN building b ON u.building = b.id JOIN billing_master bm ON u.billing_master_id  = bm.id WHERE u.created_at <= CURDATE() +'00:00:00' AND u.created_at >= CURDATE()-1 +'00:00:00' AND bm.amount < 0;" , function(err, results) {
+        if (err)
+        {
+            throw err;
+        }
+        res.json(results);
+    });
+});
 router.get('*', function (req, res, next) {
     res.send('Error 404!!!');
 });
@@ -83,3 +125,5 @@ router.post('*', function (req, res, next) {
 
 
 module.exports = router ;
+
+
